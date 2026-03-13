0cd23ea752dedec3ec2c99ef48c8fce82d5fc3b5
crismelendez888 <cris.melendez@upr.edu>
2026-03-12 22:08:58 -0400

    New texture for DyEnemy

--- src/Entities/Enemies/DyEnemy.hpp
+++ src/Entities/Enemies/DyEnemy.hpp
@@ -2,18 +2,30 @@
 #include "Enemy.hpp"
 
 class DyEnemy : public Enemy {
-    private: 
-        float angle = 135;
-        float aimAngle = 225;
-        bool loop = false;
+private:
+    float angle = 135;
+    float aimAngle = 225;
+    bool loop = false;
 
-    public:
-        DyEnemy(float x, float y) : Enemy(x, y) { 
-            this->cooldown = GetRandomValue(90, 300);
-            this->health = 1; 
+    Rectangle spriteRect;
+
+public:
+    DyEnemy(float x, float y) : Enemy(x, y) {
+        this->cooldown = GetRandomValue(90, 300);
+        this->health = 1;
+
+        
+        int r = GetRandomValue(0, 1);
+
+        if (r == 0) {
+            spriteRect = Rectangle{2, 128, 13, 14};   
+        } 
+        else {
+            spriteRect = Rectangle{2, 147, 13, 13};   
         }
+    }
 
-        void draw() override;
-        void update(std::pair<float, float> pos, HitBox target) override;
-        void attack(HitBox target) override;
+    void draw() override;
+    void update(std::pair<float, float> pos, HitBox target) override;
+    void attack(HitBox target) override;
 };
